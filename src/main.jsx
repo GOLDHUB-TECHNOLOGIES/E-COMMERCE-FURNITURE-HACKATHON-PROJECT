bba0fdeae1741648f5e474e0f39a2ada75772bd3
GOLDHUB-TECHNOLOGIES <Oluwatobilobaawoyemi@gmail.com>
2025-07-21 05:52:23 +0100

    Fix GitHub Pages config and routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CartProvider } from "./components/CartContext.jsx";
 import { UserContext, UserProvider } from "./userContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <CartProvider>
+ <HashRouter>
+  <CartProvider>
       <StrictMode>
         <UserProvider>
           <App />
         </UserProvider>
       </StrictMode>
     </CartProvider>
-  </BrowserRouter>
+ </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/furniture-project/", // 👈 replace with your repo name
+  base: "/E-COMMERCE-FURNITURE-HACKATHON-PROJECT/", // 👈 replace with your repo name
   plugins: [react()],
 });

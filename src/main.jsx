cb407d9e7e879475f69d1f8e9df9c218aca44f66
GOLDHUB-TECHNOLOGIES <Oluwatobilobaawoyemi@gmail.com>
2025-07-21 10:55:09 +0100

    Latest

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./components/CartContext.jsx";
 import { UserContext, UserProvider } from "./userContext.jsx";
 
 createRoot(document.getElementById("root")).render(
- <HashRouter>
-  <CartProvider>
+  <BrowserRouter>
+    <CartProvider>
       <StrictMode>
         <UserProvider>
           <App />
         </UserProvider>
       </StrictMode>
     </CartProvider>
- </HashRouter>
+  </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/E-COMMERCE-FURNITURE-HACKATHON-PROJECT/", // 👈 replace with your repo name
   plugins: [react()],
 });
